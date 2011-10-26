4d5e454b64f42afd3f8e6c734517bed2ae7f8876
Jon Paul <shikaga@gmail.com>
2011-10-26 11:20:25 +0100

    Moved getBestMoves and added some sketchy commands to run the python GnuGO handler

--- src/GetBestMoves.java
+++ src/GetBestMoves.java
@@ -1,7 +1,6 @@
-package Server;
-
-
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -12,13 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class getBestMoves
  */
-public class getBestMoves extends HttpServlet {
+public class GetBestMoves extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String workspaceLoction = "C:\\Users\\Shikaga7\\workspace\\GnuGoTutor"; 
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public getBestMoves() {
+    public GetBestMoves() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,8 +27,30 @@ public class getBestMoves extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+		Runtime r = Runtime.getRuntime();
+		//Process p = r.exec("cmd /c dir");
+		String execString = "python " + workspaceLoction + "\\GnuGO\\moveExtractor.py";
+		System.out.println(execString);
+		Process p = r.exec(execString);
+//		Process p = r.exec("python C:\\GnuGO\\temp.py");
+		p.waitFor();
+		BufferedReader reader=new BufferedReader(new InputStreamReader(p.getInputStream())); 
+		String line=reader.readLine(); 
+		while(line!=null) 
+		{ 
+		System.out.println(line); 
+		line=reader.readLine(); 
+		} 
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+		
+		String returnMessage = "{\"Hello\": \"Goodbye\"}";
+		System.out.println(returnMessage);
 		PrintWriter out = response.getWriter();
-	    out.println("Hello, world!");
+	    out.println(returnMessage);
 	    out.close();
 	}
 
